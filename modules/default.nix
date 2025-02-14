489bc4df1f4e1ed539ef1afdd6df0d21183836f1
uhkay <konstantius.kevin@gmail.com>
2025-02-14 20:03:26 +0700

    simplify configuration.nix

---
+++ modules/default.nix
@@ -0,0 +1,12 @@
+{
+  imports = [
+    ./audio.nix
+    ./boot.nix
+    ./gnome.nix
+    ./locale.nix
+    ./net.nix
+    ./nix.nix
+    ./ssh.nix
+    ./timezone.nix
+  ]
+}
---
+++ modules/locale.nix
@@ -0,0 +1,4 @@
+{
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+}

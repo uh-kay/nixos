150adb3702d0fc9be8b071597a2d9f720a210367
uhkay <konstantius.kevin@gmail.com>
2025-02-14 21:26:43 +0700

    update dconf

--- home-manager/modules/dconf.nix
+++ home-manager/modules/dconf.nix
@@ -1,6 +1,16 @@
 {
   dconf = {
     enable = true;
-    settings."org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
+    settings = {
+      "org/gnome/desktop/wm/preferences" = {
+        button-layout = ":minimize,maximize,close";
+      };
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+      "org/gnome/settings-daemon/plugins/power" = {
+        sleep-inactive-ac-type = "nothing";
+      };
+    };
   };
 }
